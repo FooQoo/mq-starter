2e38722df2c660ca58d894f75b3c936636fa207c
fooqoo <horseshoes.buzz.17@gmail.com>
2024-10-27 08:05:16 +0900

    feat: apply reactor

--- src/main/java/com/dxjunkyard/reststarter/presentation/consumer/LogConsumer.java
+++ src/main/java/com/dxjunkyard/reststarter/presentation/consumer/LogConsumer.java
@@ -1,17 +1,32 @@
 package com.dxjunkyard.reststarter.presentation.consumer;
 
-import java.util.function.Consumer;
+import io.micrometer.observation.ObservationRegistry;
+import java.util.function.Function;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import reactor.core.observability.micrometer.Micrometer;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Component("logConsumer")
 @RequiredArgsConstructor
 @Slf4j
-public class LogConsumer implements Consumer<String> {
+public class LogConsumer implements Function<Flux<String>, Mono<Void>> {
+
+    private final ObservationRegistry registry;
 
     @Override
-    public void accept(String payload) {
-        log.info("payload: {}", payload);
+    public Mono<Void> apply(Flux<String> stringFlux) {
+        return stringFlux
+                .delayUntil(this::process)
+                .then();
+    }
+
+    private Mono<Void> process(String string) {
+        return Mono.just(string)
+                .doOnNext(s -> log.info("string: {}", s))
+                .tap(Micrometer.observation(registry))
+                .then();
     }
 }
--- src/main/java/com/dxjunkyard/reststarter/presentation/consumer/MemberConsumer.java
+++ src/main/java/com/dxjunkyard/reststarter/presentation/consumer/MemberConsumer.java
@@ -2,22 +2,35 @@ package com.dxjunkyard.reststarter.presentation.consumer;
 
 import com.dxjunkyard.reststarter.application.service.MemberService;
 import com.dxjunkyard.reststarter.presentation.dto.payload.MemberPayload;
-import java.util.function.Consumer;
+import io.micrometer.observation.ObservationRegistry;
+import java.util.function.Function;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import reactor.core.observability.micrometer.Micrometer;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Component("memberConsumer")
 @RequiredArgsConstructor
 @Slf4j
-public class MemberConsumer implements Consumer<MemberPayload> {
+public class MemberConsumer implements Function<Flux<MemberPayload>, Mono<Void>> {
 
     private final MemberService memberService;
 
+    private final ObservationRegistry registry;
+
     @Override
-    public void accept(MemberPayload memberPayloadFlux) {
-        memberService.getMemberName(memberPayloadFlux.id())
+    public Mono<Void> apply(Flux<MemberPayload> memberPayloadFlux) {
+        return memberPayloadFlux
+                .delayUntil(this::process)
+                .then();
+    }
+
+    private Mono<Void> process(MemberPayload memberPayload) {
+        return memberService.getMemberName(memberPayload.id())
                 .doOnNext(name -> log.info("name: {}", name))
-                .subscribe();
+                .tap(Micrometer.observation(registry))
+                .then();
     }
 }
